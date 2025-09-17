8c183bb6144cc1c14136e2163eb081138f35db44
unknown <dangtruong3122005@gmail.com>
2025-09-17 23:23:20 +0700

    feat : base client interface

--- src/main/java/com/example/laptopshop_project/config/WebMvcConfig.java
+++ src/main/java/com/example/laptopshop_project/config/WebMvcConfig.java
@@ -30,7 +30,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/resources/image/");
+        registry.addResourceHandler("/client/**").addResourceLocations("/resources/client/");
 
 
     }
