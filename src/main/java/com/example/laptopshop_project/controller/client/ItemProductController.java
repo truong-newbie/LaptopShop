a31615d743523e4db3d617ecf2ddb5f89b4f4eea
unknown <dangtruong3122005@gmail.com>
2025-09-24 01:14:57 +0700

    feat: complete detail product

--- src/main/java/com/example/laptopshop_project/controller/client/ItemProductController.java
+++ src/main/java/com/example/laptopshop_project/controller/client/ItemProductController.java
@@ -1,6 +1,7 @@
 package com.example.laptopshop_project.controller.client;
 
 
+import com.example.laptopshop_project.domain.Products;
 import com.example.laptopshop_project.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemProductController {
+    private final ProductService productService;
+    public ItemProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/product/{id}")
     public String getDetailProduct(Model model, @PathVariable long id){
+        Products product = productService.getProductById(id).get();
+        model.addAttribute("product", product);
+        model.addAttribute("id", id);
         return "client/product/detail";
     }
 
