692962a5c055cd8ed180573e8712eccb772eaab1
unknown <dangtruong3122005@gmail.com>
2025-10-04 17:31:04 +0700

    feat: add carddetail interface

--- src/main/java/com/example/laptopshop_project/controller/client/ItemProductController.java
+++ src/main/java/com/example/laptopshop_project/controller/client/ItemProductController.java
@@ -36,5 +36,10 @@ public class ItemProductController {
         return "redirect:/";
     }
 
+    @GetMapping("/cart")
+    public String getCardPage(Model model) {
+        return "client/cart/show";
+    }
+
 
 }
